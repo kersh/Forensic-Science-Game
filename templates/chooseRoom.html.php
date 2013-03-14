654111dc47c8b11657922b7e9f105690b80cd71a
Vladimir Shafikov <vladimir.kersh@gmail.com>
2013-03-14 20:00:11 +0000

    Created code that prints list of rooms that are available for user

--- templates/chooseRoom.html.php
+++ templates/chooseRoom.html.php
@@ -1,34 +1,16 @@
-<p>Choose room page.</p>
+<h3>Select room that you want to play:</h3>
 
-<?php 
-	echo $room_user_data[0][0];
-	echo "<br/>";
-	echo $room_user_data[0][1];
-	echo "<br/>";
-	echo $room_user_data[0][2];
-	echo "<br/>";
-	echo $room_user_data[0][3];
-
-	echo "<br/>";
-	echo "<br/>";
-
-	echo $room_user_data[1][0];
-	echo "<br/>";
-	echo $room_user_data[1][1];
-	echo "<br/>";
-	echo $room_user_data[1][2];
-	echo "<br/>";
-	echo $room_user_data[1][3];
-
-	echo "<br/>";
-	echo "<br/>";
-
-	echo $each_room_data[0][0];
-	echo "<br/>";
-	echo $each_room_data[0][1];
-	echo "<br/>";
-	echo $each_room_data[0][2];
-	echo "<br/>";
-	echo $each_room_data[0][3];
-	// var_dump($room_user_data);
-?>
+<div class="rooms_wrapper">
+	<?php for ($i = 0; isset($each_room_data[$i][1]); $i++): ?>
+		<div class="room_item">
+			<div class="room_img"><img src="<?php echo $each_room_data[$i][1]; ?>" alt=""/></div>
+			<div class="room_content">
+				<h4><?php echo $each_room_data[$i][0]; ?></h4>
+				<p class="budget">£<?php echo $each_room_data[$i][2]; ?></p>
+				<p class="spent_budget">£<?php echo $room_user_data[$i][3]; ?></p>
+				<p class="desc"><?php echo $each_room_data[$i][3]; ?></p>
+				<p class="status"><?php echo $room_user_data[$i][2]; ?></p>
+			</div>
+		</div>
+	<?php endfor; ?>
+</div>
