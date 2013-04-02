db398eea887f718f1f71b31f6f5aa2ef4ca270a7
Vladimir Shafikov <vladimir.kersh@gmail.com>
2013-04-02 15:24:22 +0100

    Added bootstrap to the project.

--- templates/footer.html.php
+++ templates/footer.html.php
@@ -3,5 +3,6 @@
 			<p>University of Wolverhampton | All Rights Reserved | 2013</p>
 		</div>
 	</div> <!-- end of #wrapper -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- templates/header.html.php
+++ templates/header.html.php
@@ -8,9 +8,11 @@
 	<meta name="description" content="">
 	<meta name="author" content="Vladimirs Safikovs (1008629)"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<link rel="stylesheet" type="text/css" href="css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!-- <link rel="stylesheet" type="text/css" href="css/normalize.css"> -->
+	<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 
 
